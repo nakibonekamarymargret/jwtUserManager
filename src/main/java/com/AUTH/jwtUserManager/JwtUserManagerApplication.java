b8a3a1f647fb0f1bb281eaf49f748b86b25b334f
704KC <nakibonekamarymargret@gmail.com>
2025-03-31 15:24:31 +0300

    Added files

---
+++ src/main/java/com/AUTH/jwtUserManager/JwtUserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.AUTH.jwtUserManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtUserManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtUserManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AUTH/jwtUserManager/JwtUserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AUTH.jwtUserManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtUserManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
